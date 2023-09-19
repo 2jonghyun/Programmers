af7b59a57af903e9f4d2535b6d4bfff072d05f6f
2jh <118319851+2jonghyun@users.noreply.github.com>
2023-09-19 16:29:44 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.07 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        answer = my_string.trim().split("[ ]+");
+        return answer;
+    }
+}
